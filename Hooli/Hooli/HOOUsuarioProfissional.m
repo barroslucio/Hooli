f42ebcbac0daf1729626e7654381475ec815959c
Edlberto Ramos <edilberto.ramos@icloud.com>
2015-06-09 09:56:52 -0400

    Adicionando DDD, RG e CPF nas classes de cadastro de profissional.

--- Hooli/Hooli/HOOUsuarioProfissional.m
+++ Hooli/Hooli/HOOUsuarioProfissional.m
@@ -9,24 +9,5 @@
 #import "HOOUsuarioProfissional.h"
 
 @implementation HOOUsuarioProfissional
--(id)initWith : (NSDictionary*) dic{
-    self = [super init];
-    
-    _nome = [dic objectForKey:@"nome"];
-    _email = [dic objectForKey:@"email"];
-    _senha = [dic objectForKey:@"senha"];
-    _endereco = [dic objectForKey:@"endereco"];
-    _cidade = [dic objectForKey:@"cidade"];
-    _uf = [dic objectForKey:@"uf"];
-    _telefone = [dic objectForKey:@"telefone"];
-    _alvenaria = FALSE;
-    _chaveiro = FALSE;
-    _eletrica = FALSE;
-    _hidraulica = FALSE;
-    _limpeza = FALSE;
-    _pintura = FALSE;
-    
-    return self;
-}
 
 @end
