cc7bd7f55a5fb2b34cb7fd7873038da28e836f09
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-06-12 08:13:13 -0400

    Criando os métodos.

---
+++ Hooli/Hooli/HOOEditUsuarioViewController.h
@@ -0,0 +1,13 @@
+//
+//  HOOEditUsuarioViewController.h
+//  Hooli
+//
+//  Created by Rodrigo DAngelo Silva Machado on 6/12/15.
+//  Copyright (c) 2015 Hooli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HOOEditUsuarioViewController : UIViewController
+
+@end
---
+++ Hooli/Hooli/HOOEditUsuarioViewController.m
@@ -0,0 +1,30 @@
+//
+//  HOOEditUsuarioViewController.m
+//  Hooli
+//
+//  Created by Rodrigo DAngelo Silva Machado on 6/12/15.
+//  Copyright (c) 2015 Hooli. All rights reserved.
+//
+
+#import "HOOEditUsuarioViewController.h"
+
+@interface HOOEditUsuarioViewController ()
+
+@end
+
+@implementation HOOEditUsuarioViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+@end
