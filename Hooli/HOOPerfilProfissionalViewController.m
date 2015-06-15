082042344816611218f379abb4d03e2372b5504c
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-06-15 10:31:52 -0400

    Classe do profissional feita.

--- Hooli/HOOPerfilProfissionalViewController.m
+++ Hooli/HOOPerfilProfissionalViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    [self buscaDadosClienteParse];
+    self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,5 +27,29 @@
     
 }
 
+// procedimento para pegar as informações do profissional no parse
+- (void)buscaDadosClienteParse
+{
+    // variável para pegar as informações
+    PFUser *user = [PFUser currentUser];
+    
+    // criando as variáveis para usar o Parse
+    NSString *endereco = [user objectForKey:@"endereco"];
+    NSString *email = [user objectForKey:@"email"];
+    NSString *senha = [user objectForKey:@"senha"];
+    NSInteger ddd = [[user objectForKey:@"ddd"] intValue];
+    NSInteger telefone = [[user objectForKey:@"telefone"] intValue];
+    NSString *cidade = [user objectForKey:@"cidade"];
+    NSString *estado = [user objectForKey:@"estado"];
+    
+    // jogando para a tela
+    [self.lbEndereco setText:endereco];
+    [self.lbEmail setText:email];
+    [self.lbSenha setText:senha];
+    [self.lbDDD setText:[NSString stringWithFormat: @"%ld", (long)ddd]];
+    [self.lbTelefone setText:[NSString stringWithFormat: @"%ld", (long)telefone]];
+    [self.lbCidade setText:cidade];
+    [self.lbEstado setText:estado];
+}
 
 @end
