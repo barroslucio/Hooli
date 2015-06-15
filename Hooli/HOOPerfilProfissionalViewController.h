47d0e5589c13a155899add61848c631005870edf
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-06-15 10:21:47 -0400

    Adicionando as variáveis no código.

---
+++ Hooli/HOOPerfilProfissionalViewController.h
@@ -0,0 +1,20 @@
+//
+//  HOOPerfilProfissionalViewController.h
+//  Hooli
+//
+//  Created by Rodrigo DAngelo Silva Machado on 6/15/15.
+//  Copyright (c) 2015 Hooli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HOOPerfilProfissionalViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *lbEmail;
+@property (weak, nonatomic) IBOutlet UILabel *lbSenha;
+@property (weak, nonatomic) IBOutlet UILabel *lbDDD;
+@property (weak, nonatomic) IBOutlet UILabel *lbTelefone;
+@property (weak, nonatomic) IBOutlet UILabel *lbCidade;
+@property (weak, nonatomic) IBOutlet UILabel *lbEstado;
+@property (weak, nonatomic) IBOutlet UILabel *lbEndereco;
+
+@end
---
+++ Hooli/HOOPerfilProfissionalViewController.m
@@ -0,0 +1,30 @@
+//
+//  HOOPerfilProfissionalViewController.m
+//  Hooli
+//
+//  Created by Rodrigo DAngelo Silva Machado on 6/15/15.
+//  Copyright (c) 2015 Hooli. All rights reserved.
+//
+
+#import "HOOPerfilProfissionalViewController.h"
+
+@interface HOOPerfilProfissionalViewController ()
+
+@end
+
+@implementation HOOPerfilProfissionalViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+@end
