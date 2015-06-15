ce3e1d4ca895b19927015c51f1607555a34fa3c7
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-06-15 10:34:37 -0400

    Transição para a tela de edição feita.

---
+++ Hooli/Hooli/HOOEditProfissionalViewController.h
@@ -0,0 +1,13 @@
+//
+//  HOOEditProfissionalViewController.h
+//  Hooli
+//
+//  Created by Rodrigo DAngelo Silva Machado on 6/15/15.
+//  Copyright (c) 2015 Hooli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HOOEditProfissionalViewController : UIViewController
+
+@end
---
+++ Hooli/Hooli/HOOEditProfissionalViewController.m
@@ -0,0 +1,29 @@
+//
+//  HOOEditProfissionalViewController.m
+//  Hooli
+//
+//  Created by Rodrigo DAngelo Silva Machado on 6/15/15.
+//  Copyright (c) 2015 Hooli. All rights reserved.
+//
+
+#import "HOOEditProfissionalViewController.h"
+
+@interface HOOEditProfissionalViewController ()
+
+@end
+
+@implementation HOOEditProfissionalViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+@end
