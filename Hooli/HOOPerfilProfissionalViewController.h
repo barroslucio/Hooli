38b992247cc90f6666ccb73bbbdaf15809321eff
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-06-15 21:03:06 -0400

    Fazendo apenas com as labels.

--- Hooli/HOOPerfilProfissionalViewController.h
+++ Hooli/HOOPerfilProfissionalViewController.h
@@ -9,5 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface HOOPerfilProfissionalViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *lbEmail;
+@property (weak, nonatomic) IBOutlet UILabel *lbSenha;
+@property (weak, nonatomic) IBOutlet UILabel *lbDDD;
+@property (weak, nonatomic) IBOutlet UILabel *lbTelefone;
+@property (weak, nonatomic) IBOutlet UILabel *lbCidade;
+@property (weak, nonatomic) IBOutlet UILabel *lbEstado;
+@property (weak, nonatomic) IBOutlet UILabel *lbEndereco;
 
 @end
