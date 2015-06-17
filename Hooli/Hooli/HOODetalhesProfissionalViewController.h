8e85fbba5e3f9e783fbce39b02c7f0ab1f8c881d
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-06-17 11:49:18 -0400

    Tela criada

---
+++ Hooli/Hooli/HOODetalhesProfissionalViewController.h
@@ -0,0 +1,13 @@
+//
+//  HOODetalhesProfissionalViewController.h
+//  Hooli
+//
+//  Created by Rodrigo DAngelo Silva Machado on 6/17/15.
+//  Copyright (c) 2015 Hooli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HOODetalhesProfissionalViewController : UIViewController
+
+@end
