b536dcaae59a16148ff118aeaf36a8ea79b78bbb
Edlberto Ramos <edilberto.ramos@icloud.com>
2015-06-17 10:48:20 -0400

    Adicionei segmentedControl ao histórico do profissional

--- Hooli/Hooli/HOOHistoricoServicosProfissionalViewController.m
+++ Hooli/Hooli/HOOHistoricoServicosProfissionalViewController.m
@@ -9,8 +9,14 @@
 #import "HOOHistoricoServicosProfissionalViewController.h"
 
 @interface HOOHistoricoServicosProfissionalViewController ()<UITableViewDataSource, UITableViewDelegate>
+{
+    int type; //gambView pra escolher qual array vai ser mostrado na tableView
+
+}
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+- (IBAction)segmentedAction:(id)sender;
 @property (nonatomic) NSArray *arrayServicosEscolhido;
 @property (nonatomic) NSArray *arrayServicosPendentes;
 
@@ -21,41 +27,89 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self initProperties];
+    [self requisicoes];
 }
 - (void)initProperties{
-//    PFQuery *queryEscolhido = [PFQuery queryWithClassName:@"Servico"];
-//    [queryEscolhido whereKey:@"proEscolhido" equalTo:[PFUser currentUser]];
-//    self.arrayServicosEscolhido = [queryEscolhido findObjects];
+    type = 0;
+}
+
+-(void)requisicoes{
     
+    //Requisição dos serviços que já foi escolhido
+    PFQuery *queryEscolhido = [PFQuery queryWithClassName:@"Servico"];
+    [queryEscolhido whereKey:@"proEscolhido" equalTo:[PFUser currentUser]];
+    
+    self.arrayServicosEscolhido = [queryEscolhido findObjects];
+    
+    //Requisição dos serviços em aguardo de aceitação
     PFQuery *queryPendentes = [PFQuery queryWithClassName:@"Proposta"];
     [queryPendentes whereKey:@"profissional" equalTo:[PFUser currentUser]];
-    self.arrayServicosPendentes = [queryPendentes findObjects];
+    NSArray *pendentesArray = [queryPendentes findObjects];
+    NSMutableArray *pendentesMutableArray = [@[] mutableCopy];
+    for (NSDictionary *objetc in pendentesArray) {
+        PFObject *proposta = (PFObject *)objetc[@"serviço"];
+        if ([self servicosEscolhidos:pendentesArray senvicoPendente:proposta]) {
+            [pendentesMutableArray addObject:objetc];
+        }
+    }
     
-    
+    self.arrayServicosPendentes = pendentesMutableArray;
+
 }
+
+//Validação se o serviço pendente já foi escolhido
+- (BOOL)servicosEscolhidos:(NSArray *)escolhidos senvicoPendente:(PFObject *)pendente{
+
+    for (NSDictionary *servico in escolhidos) {
+        if ([servico[@"objectId"] isEqual:[pendente objectId]]) {
+            return NO;
+        }
+    }
+    return YES;
+
+}
+
+
 //TABLE VIEW
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
-   
     
-    cell.detailTextLabel.text = [[self.arrayServicosPendentes[indexPath.row] objectForKey:@"valor"] stringValue];
-    
-    
-    cell.textLabel.text = [self.arrayServicosPendentes[indexPath.row] objectForKey:@"dataServico"];
+    if (type==0) {
+        
+        PFObject *servico = [PFObject objectWithClassName:@"Servico"];
+        [servico setObjectId:[self.arrayServicosEscolhido[indexPath.row] objectForKey:@"objectId"]];
+        
+        cell.textLabel.text = servico[@"dataServico"];
+        cell.detailTextLabel.text = [[self.arrayServicosEscolhido[indexPath.row] objectForKey:@"valor"] stringValue];
 
-    
+    } else {
+
+        NSDictionary *object = self.arrayServicosPendentes[indexPath.row]; //Dicionário recebe o objeto do array de propostas.
+        PFObject *servico = (PFObject *) object[@"servico"]; //PFobject recebe o campo "servico" da classe Proposta
+        PFQuery *queryServico = [PFQuery queryWithClassName:@"Servico"];
+        [queryServico whereKey:@"objectId" equalTo:[servico objectId]]; //Faz requisição do serviço que tem objectId igual ao objeto do array de propostas
+        NSArray *dataServico = [queryServico findObjects];
+        
+        cell.textLabel.text = [dataServico[0] objectForKey:@"dataServico"];
+        cell.detailTextLabel.text = [[self.arrayServicosPendentes[indexPath.row] objectForKey:@"valor"] stringValue];
+        
+    }
+
     return cell;
     
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (type==0) {
+        return self.arrayServicosEscolhido.count;
+    } else {
         return self.arrayServicosPendentes.count;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 //- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -76,4 +130,19 @@
 //}
 
 
+- (IBAction)segmentedAction:(id)sender {
+    switch (((UISegmentedControl *) sender).selectedSegmentIndex) {
+        case 0:
+            type = 0;
+            [self.tableView reloadData];
+            break;
+        case 1:
+            type = 1;
+            [self.tableView reloadData];
+            break;
+        default:
+            break;
+    }
+
+}
 @end
