eeb175c93f3db1d2097ae0812be7f6276d81cb26
Lúcio Barros <luciobarros@MacBook-Pro-de-Lucio.local>
2015-06-19 10:37:56 -0400

    jhgjh

--- Hooli/Hooli/HOOPerfilUsuarioViewController.m
+++ Hooli/Hooli/HOOPerfilUsuarioViewController.m
@@ -7,18 +7,200 @@
 //
 
 #import "HOOPerfilUsuarioViewController.h"
+#import "HOOClienteViewController.h"
+#import "HOOAgendarServicoViewController.h"
+#import "HOOCadastroProffisionalViewController.h"
+#import "HOOLoginViewController.h"
+#import "PresentingAnimationController.h"
+#import "DismissingAnimationController.h"
 
-@interface HOOPerfilUsuarioViewController ()
+@interface HOOPerfilUsuarioViewController ()<UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate>{
+    UIFloatLabelTextField *estadoTextField;
+    UIFloatLabelTextField *cidadeTextField;
+    UIFloatLabelTextField *enderecoTextField;
+    UIFloatLabelTextField *dddTextField;
+    UIFloatLabelTextField *telefoneTextField;
+    UIFloatLabelTextField *senhaTextField;
+    UIFloatLabelTextField *emailTextField;
+    
+}
+
 
 @end
 
 @implementation HOOPerfilUsuarioViewController
 
+- (IBAction)voltarButton:(id)sender {
+    HOOLoginViewController *modalVC = [self.storyboard instantiateViewControllerWithIdentifier:@"login"];
+    
+    
+    modalVC.transitioningDelegate = self;
+    
+    modalVC.modalPresentationStyle = UIModalPresentationCustom;
+    
+    [self presentViewController:modalVC animated:YES completion:nil];
+    
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self buscaDadosClienteParse];
-    self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardFrameDidChange:) name:UIKeyboardWillChangeFrameNotification object:nil];
+    
+    estadoTextField = [UIFloatLabelTextField new];
+    [estadoTextField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    estadoTextField.floatLabelActiveColor = [UIColor orangeColor];
+    estadoTextField.placeholder = @"Estado";
+    // estadoTextField.borderStyle = UITextBorderStyleLine;
+    estadoTextField.delegate = self;
+    [self.subviewEstado addSubview:estadoTextField];
+    
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[estadoTextField]-0-|"
+                                                                      options:NSLayoutFormatAlignAllBaseline
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(estadoTextField)]];
+    // Vertical
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[estadoTextField(45)]-0-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(estadoTextField)]];
+    
+    emailTextField = [UIFloatLabelTextField new];
+    [emailTextField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    emailTextField.floatLabelActiveColor = [UIColor orangeColor];
+    emailTextField.placeholder = @"Email";
+    //emailTextField.borderStyle = UITextBorderStyleLine;
+    emailTextField.delegate = self;
+    [self.subviewEmail addSubview:emailTextField];
+    
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[emailTextField]-0-|"
+                                                                      options:NSLayoutFormatAlignAllBaseline
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(emailTextField)]];
+    // Vertical
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[emailTextField(45)]-0-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(emailTextField)]];
+    
+    
+    senhaTextField = [UIFloatLabelTextField new];
+    [senhaTextField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    senhaTextField.floatLabelActiveColor = [UIColor orangeColor];
+    senhaTextField.placeholder = @"Senha";
+    //senhaTextField.borderStyle = UITextBorderStyleLine;
+    senhaTextField.delegate = self;
+    [self.subviewSenha addSubview:senhaTextField];
+    
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[senhaTextField]-0-|"
+                                                                      options:NSLayoutFormatAlignAllBaseline
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(senhaTextField)]];
+    // Vertical
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[senhaTextField(45)]-0-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(senhaTextField)]];
+    
+    
+    cidadeTextField = [UIFloatLabelTextField new];
+    [cidadeTextField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    cidadeTextField.floatLabelActiveColor = [UIColor orangeColor];
+    cidadeTextField.placeholder = @"Cidade";
+    //cidadeTextField.borderStyle = UITextBorderStyleNone;
+    cidadeTextField.delegate = self;
+    [self.subviewCidade addSubview:cidadeTextField];
+    
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[cidadeTextField]-0-|"
+                                                                      options:NSLayoutFormatAlignAllBaseline
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(cidadeTextField)]];
+    // Vertical
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[cidadeTextField(45)]-0-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(cidadeTextField)]];
+    
+    dddTextField = [UIFloatLabelTextField new];
+    [dddTextField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    dddTextField.floatLabelActiveColor = [UIColor orangeColor];
+    dddTextField.placeholder = @"DDD";
+    //dddTextField.borderStyle = UITextBorderStyleNone;
+    dddTextField.delegate = self;
+    [self.subviewDDD addSubview:dddTextField];
+    
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[dddTextField]-0-|"
+                                                                      options:NSLayoutFormatAlignAllBaseline
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(dddTextField)]];
+    // Vertical
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[dddTextField(45)]-0-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(dddTextField)]];
+    
+    enderecoTextField = [UIFloatLabelTextField new];
+    [enderecoTextField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    enderecoTextField.floatLabelActiveColor = [UIColor orangeColor];
+    enderecoTextField.placeholder = @"Endereço";
+    // enderecoTextField.borderStyle = UITextBorderStyleNone;
+    enderecoTextField.delegate = self;
+    [self.subviewEndereco  addSubview:enderecoTextField];
+    
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[enderecoTextField]-0-|"
+                                                                      options:NSLayoutFormatAlignAllBaseline
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(enderecoTextField)]];
+    // Vertical
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[enderecoTextField(45)]-0-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(enderecoTextField)]];
+    
+    telefoneTextField = [UIFloatLabelTextField new];
+    [telefoneTextField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    telefoneTextField.floatLabelActiveColor = [UIColor orangeColor];
+    telefoneTextField.placeholder = @"Telefone";
+    //telefoneTextField.borderStyle = UITextBorderStyleLine;
+    telefoneTextField.delegate = self;
+    [self.subviewTelefone addSubview:telefoneTextField];
+    
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[telefoneTextField]-0-|"
+                                                                      options:NSLayoutFormatAlignAllBaseline
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(telefoneTextField)]];
+    // Vertical
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[telefoneTextField(45)]-0-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(telefoneTextField)]];
+    
+    
+    
+    
+    
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(ocultaTeclado:)];
+    [tapGesture setNumberOfTouchesRequired:1];
+    [[self view] addGestureRecognizer:tapGesture];
+    
+    self.pickerView = [[UIPickerView alloc] initWithFrame:CGRectMake(100, 100, 300, 100)];
+    self.pickerView.dataSource = self;
+    self.pickerView.delegate = self;
+    self.pickerView.showsSelectionIndicator = YES;
+    estadoTextField.inputView = self.pickerView;
+    
+    self.arrayUF = @[@"Acre", @"Alagoas", @"Amazonas", @"Amapá", @"Bahia", @"Ceará", @"Espírito Santo", @"Goiás", @"Maranhão", @"Minas Gerais", @"Mato Grosso do Sul", @"Mato Grosso", @"Pará", @"Paraíba", @"Pernambuco", @"Piauí", @"Paraná", @"Rio de Janeiro", @"Rio Grande do Norte", @"Rondônia", @"Roraima", @"Rio Grande do Sul", @"Santa Catarina", @"Sergipe", @"São Paulo", @"Tocantins"];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,29 +209,158 @@
     
 }
 
-// procedimento para pegar as informações do cliente no parse
-- (void)buscaDadosClienteParse
-{
-    // variável para pegar as informações
-    PFUser *user = [PFUser currentUser];
-    
-    // criando as variáveis para usar o Parse
-    NSString *endereco = [user objectForKey:@"endereco"];
-    NSString *email = [user objectForKey:@"email"];
-    NSString *senha = [user objectForKey:@"senha"];
-    NSInteger ddd = [[user objectForKey:@"ddd"] intValue];
-    NSInteger telefone = [[user objectForKey:@"telefone"] intValue];
-    NSString *cidade = [user objectForKey:@"cidade"];
-    NSString *estado = [user objectForKey:@"estado"];
-    
-    // jogando para a tela
-    [self.respEnderecoLabel setText:endereco];
-    [self.respEmailLabel setText:email];
-    [self.respSenhaLabel setText:senha];
-    [self.respDDDLabel setText:[NSString stringWithFormat: @"%ld", (long)ddd]];
-    [self.respTelefoneLabel setText:[NSString stringWithFormat: @"%ld", (long)telefone]];
-    [self.respCidadeLabel setText:cidade];
-    [self.respEstadoLabel setText:estado];
+//PIKER VIEW - ESTADOS
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
 }
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    return [self.arrayUF count];
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row
+            forComponent:(NSInteger)component
+{
+    estadoTextField.text = @"Acre" ;
+    return [self.arrayUF objectAtIndex:row];
+    
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    estadoTextField.text=[self.arrayUF objectAtIndex:row];
+}
+
+//ALERTA DO STATUS DE CADASTRO
+- (void)alertStatusCadastro:(NSString *)status
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Alerta!" message:status delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alert show];
+    
+}
+
+
+// procedimento para o teclado ser ocultado
+- (void)ocultaTeclado:(UITapGestureRecognizer *)sender
+{
+    [emailTextField resignFirstResponder];
+    [senhaTextField resignFirstResponder];
+    [telefoneTextField resignFirstResponder];
+    [cidadeTextField resignFirstResponder];
+    [estadoTextField resignFirstResponder];
+    [enderecoTextField resignFirstResponder];
+    [dddTextField resignFirstResponder];
+    
+}
+
+- (id <UIViewControllerAnimatedTransitioning>)animationControllerForPresentedController:(UIViewController *)presented presentingController:(UIViewController *)presenting sourceController:(UIViewController *)source
+{
+    return [[PresentingAnimationController alloc] init];
+}
+
+- (id <UIViewControllerAnimatedTransitioning>)animationControllerForDismissedController:(UIViewController *)dismissed
+{
+    return [[DismissingAnimationController alloc] init];
+}
+
+
+
+- (void)cadastraClienteParse{
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc]init];
+    NSNumber *telefone = [formatter numberFromString:telefoneTextField.text];
+    NSNumber *ddd = [formatter numberFromString:dddTextField.text];
+    NSNumber *tipo = [NSNumber numberWithInt:0];;
+    
+    PFUser *user = [PFUser user];
+    user.username = emailTextField.text;
+    user.password = senhaTextField.text;
+    
+    
+    user[@"endereco"] = enderecoTextField.text;
+    user[@"email"]=emailTextField.text;
+    user[@"estado"] = estadoTextField.text;
+    user[@"cidade"] = cidadeTextField.text;
+    user[@"ddd"] = ddd;
+    user[@"telefone"] = telefone;
+    user[@"tipo"] = tipo;
+    
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error)
+        {   // Hooray! Let them use the app now.
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Cadastro bem sucedido"
+                                                                message:@"Obrigado!"
+                                                               delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
+            [self segueViewController];
+        } else
+        {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Sorry!"
+                                                                message:[error.userInfo objectForKey:@"error"]
+                                                               delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
+            
+        }
+    }];
+}
+
+// salvando os dados dos clientes
+- (IBAction)saveButton:(id)sender
+{
+    NSString *statusCadastro;
+    
+    //VERIFICA SE AS TEXTFILDS ESTÃO TODAS PREENCHIDAS
+    if (![dddTextField.text isEqualToString:@""] && ![emailTextField.text isEqualToString:@""] && ![senhaTextField.text isEqualToString:@""]  && ![cidadeTextField.text isEqualToString:@""] && ![estadoTextField.text isEqualToString:@""] && ![telefoneTextField.text isEqualToString:@""] && ![enderecoTextField.text isEqualToString:@""])
+    {
+        //Método para salvar no Parse.com
+        [self cadastraClienteParse];
+    }
+    
+    else
+    {
+        statusCadastro = @"Preencha todos os campos";
+        [self alertStatusCadastro:statusCadastro];
+    }
+    
+    
+}
+
+
+
+- (void)segueViewController{
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    HOOAgendarServicoViewController *viewController = (HOOAgendarServicoViewController *)[storyboard instantiateViewControllerWithIdentifier:@"User"];
+    [self presentViewController:viewController animated:YES completion:nil];
+    
+}
+
+- (void)keyboardFrameDidChange:(NSNotification *)notification
+{
+    
+    
+    CGRect keyboardEndFrame = [[[notification userInfo] objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    CGRect keyboardBeginFrame = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+    UIViewAnimationCurve animationCurve = [[[notification userInfo] objectForKey:UIKeyboardAnimationCurveUserInfoKey] integerValue];
+    NSTimeInterval animationDuration = [[[notification userInfo] objectForKey:UIKeyboardAnimationDurationUserInfoKey] integerValue];
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    [UIView setAnimationCurve:animationCurve];
+    
+    CGRect newFrame = self.view.frame;
+    CGRect keyboardFrameEnd = [self.view convertRect:keyboardEndFrame toView:nil];
+    CGRect keyboardFrameBegin = [self.view convertRect:keyboardBeginFrame toView:nil];
+    
+    newFrame.origin.y -= (keyboardFrameBegin.origin.y - keyboardFrameEnd.origin.y);
+    self.view.frame = newFrame;
+    
+    [UIView commitAnimations];
+    
+    
+}
+
+
 
 @end
